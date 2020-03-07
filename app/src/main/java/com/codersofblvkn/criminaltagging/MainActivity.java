cd292b7382980d596d06be4a72ecb330dfc5d380
Niveth Saran <31360464+nivethsaran@users.noreply.github.com>
2020-03-07 22:57:55 +0530

    Another Commit

--- app/src/main/java/com/codersofblvkn/criminaltagging/MainActivity.java
+++ app/src/main/java/com/codersofblvkn/criminaltagging/MainActivity.java
@@ -1,46 +1,60 @@
 package com.codersofblvkn.criminaltagging;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-import com.mindorks.paracamera.Camera;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.content.Intent.ACTION_PICK;
+import static android.os.Environment.getExternalStoragePublicDirectory;
+
 
 public class MainActivity extends AppCompatActivity {
-Button button;
-    Camera camera;
-
+private static final String TAG="ALERT";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button=findViewById(R.id.button);
 
-// Build the camera
-        camera = new Camera.Builder()
-                .resetToCorrectOrientation(true)// it will rotate the camera bitmap to the correct orientation from meta data
-                .setTakePhotoRequestCode(1)
-                .setDirectory("pics")
-                .setName("ali_" + System.currentTimeMillis())
-                .setImageFormat(Camera.IMAGE_JPEG)
-                .setCompression(75)
-                .setImageHeight(1000)// it will try to achieve this height as close as possible maintaining the aspect ratio;
-                .build(this);
-
-        button.setOnClickListener(new View.OnClickListener() {
+        FirebaseMessaging.getInstance().subscribeToTopic("alert").addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
-            public void onClick(View view) {
-                try {
-                    camera.takePicture();
-                }catch (Exception e){
-                    e.printStackTrace();
+            public void onComplete(@NonNull Task<Void> task) {
+                String msg = getString(R.string.msg_subscribed);
+                if (!task.isSuccessful()) {
+                    msg = getString(R.string.msg_subscribe_failed);
                 }
-
-                
+                Log.d(TAG, msg);
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
             }
         });
 
     }
+
+
 }
